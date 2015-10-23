9c60425916c07bee2c4b6fd85ef9ff8015cad05e
Alexander Suevalov <suevalov.work@gmail.com>
2015-10-23 08:26:22 +0300

    Added DataArt link

--- presentation/slides/final-slide.jsx
+++ presentation/slides/final-slide.jsx
@@ -15,6 +15,7 @@ export default class FinalSlide extends React.Component {
         <Heading size={3} textColor='primary'>Спасибо</Heading>
         <div className='thanksSlide__links'>
           <a href="http://suevalov.com/" target="__blank">Александр Суевалов</a>
+          <a href="http://dataart.com/" target="__blank">DataArt</a>
           <a href="mailto:suevalov.me@gmail.com">suevalov.me@gmail.com</a>
           <a href="https://github.com/suevalov" target="__blank" className="github">http://github.com/suevalov</a>
           <a href="https://twitter.com/Suevalov" target="__blank" className="twitter">http://twitter.com/Suevalov</a>
